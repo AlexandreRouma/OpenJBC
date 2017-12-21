2834c89eab30ceb3781ed81cdbf82a1f689cb83d
AlexandreRouma <alexandre.rouma@gmail.com>
2017-12-21 15:30:18 +0100

    Created AVR Project and added some missing parts

---
+++ AVR Code/OpenJBC/OpenJBC/main.c
@@ -0,0 +1,18 @@
+/*
+ * OpenJBC.c
+ *
+ * Created: 21/12/2017 15:19:24
+ * Author : Alex
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
